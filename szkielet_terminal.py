26e419510e1e27b5ae3c9399b969e8683b184c3a
Pola A. Mikolajczak <mikolajczak.pola@gmail.com>
2014-04-24 13:21:52 +0200

    puste funkcje w terminalu
    dodane, czekaja na wypelnienie

--- szkielet_terminal.py
+++ szkielet_terminal.py
@@ -1,3 +1,18 @@
+def sub(id):
+    return
+def unsub(id):
+    return
+def add_pub(json):
+    return
+def update_pub(id, json):
+    return
+def replace_pub(id, json):
+    return
+def delete_pub(id):
+    return
+    
+
+
 exit = 0;
 while exit == 0:
     print("Hi, user! Choose one option from the list below:");
@@ -5,7 +20,7 @@ while exit == 0:
     print("2 - to unsubcribe a publication");
     print("3 - to add a publication");
     print("4 - to update a publication");
-    print("5 - to raplace a publication");
+    print("5 - to replace a publication");
     print("6 - to delete a publication");
     print("0 - to exit")
 
@@ -13,18 +28,26 @@ while exit == 0:
     print(menu)
     if menu == "1":
         pub_id = input("publication id:")
-
+        sub(pub_id)
     elif menu == "2":
         pub_id = input("publication id:")
+        unsub(pub_id)
     elif menu == "3":
-        pub_id = input("publication id:")
+        json = input("json:")
+        add_pub(json)
     elif menu == "4":
         pub_id = input("publication id:")
+        json = input("json:")
+        update_pub(pub_id, json)
     elif menu == "5":
         pub_id = input("publication id:")
+        json = input("json:")
+        replace_pub(pub_id, json)
     elif menu == "6":
         pub_id = input("publication id:")
+        delete_pub(pub_id)
     elif menu == "0":
         exit = 1;
     else:
         print("Sorry, I don't know what do you watn to do.")
+
