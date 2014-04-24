d1954a9a0571ad610be9b44fdc9b4546a766935a
Kuba Orlik <groovy354@gmail.com>
2014-04-24 14:26:02 +0200

    handle disconnecting clients

--- temp_server.py
+++ temp_server.py
@@ -22,7 +22,12 @@ def monitorClients():
             ready_to_read, ready_to_write, in_error = select.select(connected_sockets, [], [], 1)
             for socket in ready_to_read:
                 client = clientCollection.getBySocket(socket)
-                data = socket.recv(1024)
+                try:
+                    data = socket.recv(1024)
+                except ConnectionAbortedError:
+                    connected_sockets.remove(socket)
+                    print("client", client.id, "disconnected")
+                    continue
                 client.parseIncomingJSON(data)
                 client.respond(200, "ok", "ok")
 
@@ -30,8 +35,11 @@ def monitorClients():
 class ClientCollection():
     def __init__(self):
         self.clients=[]
+        self.total_clients=0
     def addClient(self, client):
         self.clients.append(client)
+        client.id=self.total_clients
+        self.total_clients+=1
         connected_sockets.append(client.socket)
     def getBySocket(self, socket):
         for client in self.clients:
@@ -42,22 +50,25 @@ class Client():
     def __init__(self, socket, address):
         self.socket = socket
         self.address = address
+        self.id = None
     def parseIncomingJSON(self, string):
         string = str(string, encoding='UTF-8')
         print(string)
         try:
             json_temp = json.loads(string)
-            json_correct = true
+            json_correct = True
         except ValueError:
             print("badly formatted json!")
-            json_correct = false
+            json_correct = False
         if json_correct:
             verb_present = 'verb' in json_temp
             print("verb_present:", verb_present)
             attributes_present = 'attributes' in json_temp
             if verb_present & attributes_present:
                 self.do(verb, attributes)
-            
+    def do(self, verb, attrbiutes):
+        if verb=="sub":
+            print("create subscriptions")
     def respond(self, res_number, status, message):
             message = json.dumps({'res_number': res_number, 'status': status, 'message': message})
             self.socket.send(bytes(message, "UTF-8"))
