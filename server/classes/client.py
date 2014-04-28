11617a3fa65fa358975640064fe6ab16ebeae9b6
Kuba Orlik <groovy354@gmail.com>
2014-04-28 12:44:23 +0200

    attempt - optional RequestID handling
    in preparation to create a proper client

--- server/classes/client.py
+++ server/classes/client.py
@@ -52,12 +52,16 @@ class Client():
         if verb_present & attributes_present:
             verb = json_temp["verb"]
             attributes = json_temp["attributes"]
-            self.do(verb, attributes)
+            if "request_id" in json_temp:
+                request_id=json_temp["attributes"]
+            else:
+                request_id=None
+            self.do(verb, attributes, request_id)
         else:
             self.reportBadSyntax()
 
-    def reportBadSyntax(self, message=None):
-        if message==None:
+    def reportBadSyntax(self, message=None, request_id=None):
+        if message==None or message=="":
             message="bad request syntax"
         self.respond(300, "error", message)
 
@@ -67,97 +71,97 @@ class Client():
                 return True
         return False
 
-    def do(self, verb, attributes):
+    def do(self, verb, attributes, request_id):
         if verb=="sub":
-            self.__sub(attributes)
+            self.__sub(attributes, request_id)
         elif verb=="new_pub":
-            self.__new_pub(attributes) 
+            self.__new_pub(attributes, request_id) 
         elif verb=="delete_pub":
-            self.__delete_pub(attributes)
+            self.__delete_pub(attributes, request_id)
         elif verb=="update_pub":
-            self.__update_pub(attributes)
+            self.__update_pub(attributes, request_id)
         elif verb=="replace_pub":
-            self.__replace_pub(attributes)
+            self.__replace_pub(attributes, request_id)
         else:
             print("bad verb")
             self.respond(300, "error", "unknown verb")
 
-    def __sub(self, attributes):
+    def __sub(self, attributes, request_id):
         print("handling SUB request")
         if not validator.attributesPresent(["id", "pub_name"], attributes):
-            self.reportBadSyntax()
+            self.reportBadSyntax("", request_id)
             return
         hasSub = self.hasSubID(attributes["id"])
         if hasSub:
-            self.respond(422, "error", "you already have that id assigned to a publication. Unsub first or choose a different ID")
+            self.respond(422, "error", "you already have that id assigned to a publication. Unsub first or choose a different ID", request_id)
             return
         try:
             pub = publication.Collection.getPublicationByName(attributes["pub_name"])
         except PubNotFoundError:
-            self.respond(404, "error", "publication not found")
+            self.respond(404, "error", "publication not found", request_id)
             return
         sub = subscription.Collection.new(self, pub, attributes["id"])
         self.subscriptions.append(sub)
-        self.respondOK(pub.getContents())
+        self.respondOK(pub.getContents(), request_id)
 
-    def __new_pub(self, attributes):
+    def __new_pub(self, attributes, request_id):
         if not validator.attributesPresent(["name", "content"], attributes):
-            self.reportBadSyntax("some attributes missing")
+            self.reportBadSyntax("some attributes missing", request_id)
             return  
         if publication.Collection.nameTaken(attributes["name"]):
-            self.respond(420, "error", "publication name already exists")
+            self.respond(420, "error", "publication name already exists", request_id)
             return
         if not validator.isCorerctJSON(attributes["content"]):
-            self.reportBadSyntax("incorrect json in content")
+            self.reportBadSyntax("incorrect json in content", request_id)
             return
         publication.Collection.createPublication(attributes["name"], attributes["content"])
-        self.respondOK("")
+        self.respondOK(None,  request_id)
 
-    def __delete_pub(self, attributes):
+    def __delete_pub(self, attributes, request_id):
         if not validator.attributesPresent(["name"], attributes):
-            self.reportBadSyntax("'name' attribute missing")
+            self.reportBadSyntax("'name' attribute missing", request_id)
             return  
         if not publication.Collection.nameTaken(attributes["name"]):
-            self.respond(420, "error", "publication name does not exist")
+            self.respond(420, "error", "publication name does not exist", request_id)
             return
         publication.Collection.delete(attributes["name"])
-        self.respondOK(200)
+        self.respondOK("ok", request_id)
 
-    def __update_pub(self, attributes):
+    def __update_pub(self, attributes, request_id):
         if not validator.attributesPresent(["pub_name", "changes"], attributes):
-            self.reportBadSyntax("some attributes missing")
+            self.reportBadSyntax("some attributes missing",request_id)
             return          
         if not publication.Collection.nameTaken(attributes["pub_name"]):
-            self.respond(404, "error", "publication name not found")
+            self.respond(404, "error", "publication name not found", request_id)
             return
         if not isinstance(attributes["changes"], list):
-            self.reportBadSyntax("'changes' attribute must be a [] list")
+            self.reportBadSyntax("'changes' attribute must be a [] list",request_id)
             return
         patch = jsonpatch.JsonPatch(attributes["changes"])
         pub = publication.Collection.getPublicationByName(attributes['pub_name'])
         pub.applyPatch(patch)
-        self.respondOK(pub.getContents())
+        self.respondOK(pub.getContents(), request_id)
         pub.propagate("update")
 
-    def __replace_pub(self, attributes):
+    def __replace_pub(self, attributes, request_id):
         if not validator.attributesPresent(["pub_name", "new_content"], attributes):
-            self.reportBadSyntax("some attributes missing")
+            self.reportBadSyntax("some attributes missing",request_id)
             return   
         if not publication.Collection.nameTaken(attributes["pub_name"]):
-            self.respond(404, "error", "publication name not found")
+            self.respond(404, "error", "publication name not found",request_id)
             return
         pub = publication.Collection.getPublicationByName(attributes["pub_name"])
         pub.setContentByObject(attributes["new_content"])
-        self.respondOK(pub.getContents())
+        self.respondOK(pub.getContents(),request_id)
         pub.propagate("replace")
 
-    def respond(self, res_number, status, message):
-            message = json.dumps({'res_number': res_number, 'status': status, 'message': message})
+    def respond(self, res_number, status, message, request_id):
+            message = json.dumps({'res_number': res_number, 'status': status, 'message': message, "request_id":request_id })
             print("sending", message)
             self.socket.send(bytes(message, "UTF-8"))
 
-    def respondOK(self, message):
-        self.respond(200, "ok", message)
+    def respondOK(self, message, request_id):
+        self.respond(200, "ok", message, request_id)
 
     def unsubAll(self):
         for sub in self.subscriptions:
